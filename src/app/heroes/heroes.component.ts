ec386f28f53b2aea30fe74cf559f23938f066362
zyunhuku kou <ekqoql6418@yahoo.co.jp>
2023-05-04 21:29:57 +0900

    05042130

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
-  @Component({
+@Component({
     selector: 'app-heroes',
     templateUrl: './heroes.component.html',
-    styleUrls: ['./heroes.component.css'] 
-  })
+    styleUrls: [ './heroes.component.css' ]
+})
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
